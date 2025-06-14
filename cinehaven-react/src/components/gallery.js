2934175e7391035c08e840101caf3f7d1c495ae9
Jethrodanebadar <jetdbadar@gmail.com>
2025-06-13 22:18:36 -0400

    Finished the draft of Homepage

---
+++ cinehaven-react/src/components/gallery.js
@@ -0,0 +1,24 @@
+const imageData = [
+  {
+    title: "flowers",
+    url: "img/flowers.jpg",
+  },
+  {
+    title: "heart",
+    url: "img/heart.jpg",
+  },
+  {
+    title: "invitation",
+    url: "img/invitation.jpg",
+  },
+  {
+    title: "table",
+    url: "img/table.jpg",
+  },
+  {
+    title: "testimony",
+    url: "img/testimony.jpg",
+  },
+];
+
+export default imageData;
--- cinehaven-react/src/components/header.jsx
+++ cinehaven-react/src/components/header.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <>
       <header>
@@ -6,8 +10,40 @@ const Header = () => {
         <nav>
           <a href="#">Home</a>
           <a href="#">About</a>
-          <a href="#">Portfolio</a>
+          <a href="#">Experience</a>
         </nav>
+        <button onClick={() => setOpen(!isOpen)}>
+          {isOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="20"
+              height="20"
+              viewBox="0 0 50 50"
+            >
+              <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="20"
+              height="20"
+              viewBox="0 0 50 50"
+            >
+              <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+            </svg>
+          )}
+        </button>
+        {isOpen && (
+          <div className="modal">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Experience</a>
+          </div>
+        )}
       </header>
     </>
   );
